c577a46427512911b56df2d58d449eb0dfd94b70
Angel-Shah <angel.shah2468@gmail.com>
2021-04-25 21:33:23 -0400

    started 'Home' page + added button for extension

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,29 @@ export default function Home() {
       </Head>
       <body className="content">
         
-      <h1 className="slogan mx-auto ">Secure accounts with <div style={{fontWeight:"bold",color:"orange",fontFamily:"Arial", display:"inline"
-    }}>IMAGES</div>, not passwords.</h1>
-      <div style={{display:"flexbox", flexDirection:"row"}}>
+      <h1 className="slogan mx-5 " style={{fontSize:"3.7rem"}}>
+        Secure accounts with 
+        <div style={{fontWeight:"bold",color:"orange",fontFamily:"Arial",fontSize:"4.5rem"}}>
+        IMAGES
+        </div>
+        <i>not</i> <br/>passwords.</h1>
 
-      <div className="frosted-panel mx-3">
+        {/* <a href="https://chrome.google.com/webstore/detail/photocrypt/kmoamkcocigmimdndlpakeboolcbiddg?hl=en" target="_blank" rel="noopener noreferrer" >
+          <div className="mx-5 my-5" style={{display:"flex", flexDirection:"row",border:"1px solid orange",width:"35vw", padding:"10px",justifyContent:"center",borderRadius:"10px"}}>
+          <i className="fa fa-chrome " style={{color:"#e2c7fd",fontSize:"3rem"}}/>
+          <h2 className="mx-3 my-auto align-text-center" style={{color:"#e2c7fd",fontSize:"1.8rem",verticalAlign:"center"}}>Get PhotoCrypt For Chrome</h2>
+          </div>
+        </a> */}
 
-      </div>
-      <div className="frosted-panel mx-3">
+        <a href="https://chrome.google.com/webstore/detail/photocrypt/kmoamkcocigmimdndlpakeboolcbiddg?hl=en" target="_blank" rel="noopener noreferrer">
+        <button  type="button" class="btn btn-outline-light mx-5 p-3 py-2.5 my-5" style={{display:"flex", flexDirection:"row"}}>
+          <i className="fa fa-chrome text-align-center" style={{color:"#e2c7fd",fontSize:"3rem"}}/>
+        <h2 className="mx-3 my-auto align-text-center" style={{color:"#e2c7fd",fontSize:"1.8rem",verticalAlign:"center"}}>Get PhotoCrypt For Chrome</h2>
+         </button>
+        </a>
+        
 
-      </div>
-      </div>
+
       </body>
       
     </div>
